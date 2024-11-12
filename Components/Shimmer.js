b04a871e469b90c6a2cf9019c5dc83d00b7d3d14
Sejith <sejith.kv124578@gmail.com>
2024-11-12 23:00:37 +0530

    first commit

---
+++ Components/Shimmer.js
@@ -0,0 +1,30 @@
+const Shimmer = () => {
+    return (
+        <div class="shimmer">
+        {/* <!-- Shimmer header --> */}
+        <div class="shimmer-header">Since, this project works on Swiggy API we cannot directly use it. So, Please Download Allow CORS Extension - Click on this <a href="https://chromewebstore.google.com/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en" className="text-blue-500" target="_blank">LINK</a> to redirect to the Extension Page. </div>
+    
+        {/* <!-- Shimmer cards --> */}
+        <div class="shimmer-cards">
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+          
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+          
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+          <div class="shimmer-card"></div>
+        </div>
+
+
+        </div>
+    )
+}
+
+export default Shimmer;
